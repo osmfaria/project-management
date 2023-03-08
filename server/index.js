7a7bc9e11625be8bf792707e280178cb38f38ab8
Osmar Faria <osm.faria@gmail.com>
2023-03-08 09:50:59 -0800

    implemented express, created schemas for GraphQL and models for mongoose

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
+const { graphqlHTTP } = require('express-graphql')
 const express = require('express')
+const schema = require('./schema/schema')
+const colors = require('colors')
+const connectDB = require('./config/db')
 require('dotenv').config()
+
 const port = process.env.PORT || 5000
 
 const app = express()
+
+// connect to database
+connectDB()
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+)
 
 app.listen(port, console.log(`Server running on port ${port}`))
